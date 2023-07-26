0e2a1bfff1476aedf9d591062b5fd424ded3a678
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-07-26 18:00:55 +0300

    make camera in project

--- React_Native_First_Project/screens/main/CreatePostsScreen.js
+++ React_Native_First_Project/screens/main/CreatePostsScreen.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function CreatePostsScreen() {
    return (
       <View style={styles.container}>
-         <Text>CreateScreen</Text>
+         <View style={styles.cameraContainer}>
+            <Camera style={styles.camera}>
+               <TouchableOpacity onPress={() => {}} style={styles.cameraButton}>
+                  <FontAwesome5 name="camera" size={24} color="#BDBDBD" />
+               </TouchableOpacity>
+            </Camera>
+            <Text style={styles.cameraTitle}>Upload Photo</Text>
+         </View>
       </View>
    );
 }
@@ -12,7 +22,34 @@ export default function CreatePostsScreen() {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
+   },
+   cameraContainer: {
+      width: 343,
+      height: 267,
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginTop: 32,
+   },
+   camera: {
+      height: 240,
+      borderRadius: 8,
       justifyContent: "center",
       alignItems: "center",
+      backgroundColor: "#F6F6F6",
+      borderWidth: 1,
+      borderColor: "#E8E8E8",
+   },
+   cameraButton: {
+      borderRadius: 50,
+      height: 60,
+      width: 60,
+      backgroundColor: "#fff",
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   cameraTitle: {
+      marginTop: 8,
+      fontSize: 16,
+      color: "#BDBDBD",
    },
 });
